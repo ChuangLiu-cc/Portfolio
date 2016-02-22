7c73089fbd46ee8d629f0c761c95552ee2893221
Chuang Liu <jlbcliuchuang@gmail.com>
2016-02-21 23:19:24 -0500

    Modified old files

--- js/main.js
+++ js/main.js
@@ -1,6 +1,53 @@
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+
+
 jQuery(document).ready(function($){
 	//open-close submenu on mobile
 	$('.cd-main-nav').on('click', function(event){
 		if($(event.target).is('.cd-main-nav')) $(this).children('ul').toggleClass('is-visible');
 	});
+});
+
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('header').removeClass('cd-header').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('nav-up').addClass('cd-header');
+        }
+    }
+    
+    lastScrollTop = st;
+}
+
+$(document).ready(function() {
+  $(".fancybox")
+  .fancybox({
+    openEffect  : 'none',
+  });
 });
