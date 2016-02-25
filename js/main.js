6d09e660b026b13e1e8a10cffd878b38a478d39a
Chuang Liu <jlbcliuchuang@gmail.com>
2016-02-25 15:37:44 -0500

    Added dropdown project button and contact info

--- js/main.js
+++ js/main.js
@@ -11,6 +11,54 @@ jQuery(document).ready(function($){
 	});
 });
 
+$(".contactMe").click(function(){
+    $('html, body').animate({
+        scrollTop: $(document).height()
+    },1500);
+});
+
+$(".home").click(function(){
+    $('html, body').animate({
+        scrollTop: 0
+    },1500);
+});
+
+$("#project_1").click(function(){
+    $('html, body').animate({
+        scrollTop: $(".cd-color-1").position().top
+    },1500);
+    //$(document).scrollTop($(".cd-bg-2").y);
+});
+
+$("#project_2").click(function(){
+    $('html, body').animate({
+        scrollTop: $(".cd-color-2").position().top
+    },1500);
+    //$(document).scrollTop($(".cd-bg-2").y);
+});
+
+$("#project_3").click(function(){
+    $('html, body').animate({
+        scrollTop: $(".cd-color-3").position().top
+    },1500);
+    //$(document).scrollTop($(".cd-bg-2").y);
+});
+
+$("#project_4").click(function(){
+    $('html, body').animate({
+        scrollTop: $(".cd-color-4").position().top
+    },1500);
+    //$(document).scrollTop($(".cd-bg-2").y);
+});
+
+
+$(".about").click(function(){
+    $('html, body').animate({
+        scrollTop: $(".cd-bg-2").position().top
+    },1500);
+    
+    //$(document).scrollTop($(".cd-fixed-bg").height());
+});
 
 $(window).scroll(function(event){
     didScroll = true;
@@ -302,3 +350,49 @@ $(document).ready(function() {
   $(window).trigger('scroll');
 
 });
+
+
+$(document).ready(function() {
+
+  //window and animation items
+  var animation_elements = $.find('.animation-element1');
+  var web_window = $(window);
+
+  //check to see if any animation containers are currently in view
+  function check_if_in_view() {
+    //get current window information
+    var window_height = web_window.height();
+    var window_top_position = web_window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
+
+    //iterate through elements to see if its in view
+    $.each(animation_elements, function() {
+
+      //get the element sinformation
+      var element = $(this);
+      var element_height = $(element).outerHeight();
+      var element_top_position = $(element).offset().top;
+      var element_bottom_position = (element_top_position + element_height);
+
+      //check to see if this current container is visible (its viewable if it exists between the viewable space of the viewport)
+      if ((element_bottom_position >= window_top_position) && (element_top_position <= window_bottom_position)) {
+        element.addClass('in-view');
+      } else {
+        element.removeClass('in-view');
+      }
+    });
+
+  }
+
+  //on or scroll, detect elements in view
+  $(window).on('scroll resize', function() {
+    animation_elements.x -= 100;
+      check_if_in_view();
+    })
+    //trigger our scroll event on initial load
+  $(window).trigger('scroll');
+
+});
+
+
+
